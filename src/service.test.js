82d23478b277bd78f60c226977ea0c15a56c8074
Jonah Shirts <jshirts03@gmail.com>
2026-02-06 19:40:52 -0700

    removed bcrypt import; linter is happy now

--- src/service.test.js
+++ src/service.test.js
@@ -1,7 +1,6 @@
 const request = require('supertest');
 const app = require('./service');
 const { DB, Role } = require('./database/database.js')
-const bcrypt = require('bcrypt');
 
 let testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let userId;
