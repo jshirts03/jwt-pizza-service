548e3c1678bb375b73de0bfac77db6077600c3e7
Jonah Shirts <jshirts03@gmail.com>
2026-02-18 15:55:40 -0700

    deleteUser created for database

--- src/database/database.js
+++ src/database/database.js
@@ -154,6 +154,19 @@ class DB {
     }
   }
 
+  async deleteUser(userId) {
+    const connection = await this.getConnection();
+    try {
+      await this.query(connection, `DELETE FROM user WHERE id=?`, [userId])
+      await this.query(connection, `DELETE FROM userrole WHERE userId=?`, [userId])
+    } catch {
+        await connection.rollback();
+        throw new StatusCodeError('unable to delete franchise', 500);
+    } finally {
+      connection.end();
+    }
+  }
+
   async getOrders(user, page = 1) {
     const connection = await this.getConnection();
     try {
