6c332378dfe3c3b01313f62cb94d19378c034b4e
Jonah Shirts <jshirts03@gmail.com>
2026-03-16 15:31:38 -0600

    Metrics: memory and cpu usage work now

--- src/metrics.js
+++ src/metrics.js
@@ -1,10 +1,12 @@
 const configFile = require('./config');
 const config = configFile.metrics
+const os = require('os');
 
 // Metrics stored in memory
 const requests = {};
 let successfulAuthCount = 0;
 let failedAuthCount = 0;
+let currentCpuUsage = 0;
 
 function markSuccessfulAuth(){
     successfulAuthCount++;
@@ -14,7 +16,43 @@ function markFailedAuth(){
     failedAuthCount++;
 }
 
+const getCpuStats = () => {
+  const cpus = os.cpus();
+  let idle = 0, total = 0;
+  cpus.forEach(cpu => {
+    for (let type in cpu.times) total += cpu.times[type];
+    idle += cpu.times.idle;
+  });
+  return { idle, total };
+};
 
+// Initialize the first snapshot
+let lastStats = getCpuStats();
+
+
+setInterval(() => {
+  const currentStats = getCpuStats();
+
+  const idleDiff = currentStats.idle - lastStats.idle;
+  const totalDiff = currentStats.total - lastStats.total;
+
+  if (totalDiff > 0) {
+    const usage = 100 - (100 * idleDiff / totalDiff);
+    // Update your local variable as a clean Number
+    currentCpuUsage = Number(usage.toFixed(2));
+  }
+
+  // Store current stats as "lastStats" for the next interval calculation
+  lastStats = currentStats;
+}, 9000); // Updates every 2 seconds
+
+function getMemoryUsagePercentage() {
+  const totalMemory = os.totalmem();
+  const freeMemory = os.freemem();
+  const usedMemory = totalMemory - freeMemory;
+  const memoryUsage = (usedMemory / totalMemory) * 100;
+  return memoryUsage.toFixed(2);
+}
 
 
 // Middleware to track requests
@@ -32,6 +70,8 @@ setInterval(() => {
   });
    metrics.push(createMetric('authSuccess', successfulAuthCount, '1', 'sum', 'asInt', {}));
    metrics.push(createMetric('authFail', failedAuthCount, '1', 'sum', 'asInt', {}));
+   metrics.push(createMetric('cpuUsage', parseInt(currentCpuUsage), '%', 'gauge', 'asInt', {}));
+   metrics.push(createMetric('memoryUsage', parseInt(getMemoryUsagePercentage()), '%', 'gauge', 'asInt', {}));
 
   sendMetricToGrafana(metrics);
 }, 10000);
