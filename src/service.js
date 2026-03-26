cac537191191a629935160cce4a67ee2245a5028
Jonah Shirts <jshirts03@gmail.com>
2026-03-25 23:53:28 -0600

    Logging: added logget http tracker middleware to service

--- src/service.js
+++ src/service.js
@@ -6,11 +6,13 @@ const userRouter = require('./routes/userRouter.js');
 const version = require('./version.json');
 const config = require('./config.js');
 const metrics = require('./metrics.js')
+const logger = require("./logger.js")
 
 const app = express();
 app.use(express.json());
 app.use(metrics.requestTracker);
 app.use(metrics.latencyTracker);
+app.use(logger.httpLogger);
 app.use(setAuthUser);
 app.use(metrics.activeUserTracker);   //// THIS is HUGE;  uses the JWT service to encrypt the authorization token and adds a user field to the request that has the valid user taken from the database
                                       //// This is why all requests are sent with the "authorization: token" as the data, that's how it verifies you're a valid user
