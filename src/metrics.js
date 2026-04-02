51383e04f0854e6031c3856c9618ad8242f057d1
Jonah Shirts <jshirts03@gmail.com>
2026-04-02 11:39:06 -0600

    Metrics: fixing CPU percentage to prep for Chaos testing

--- src/metrics.js
+++ src/metrics.js
@@ -38,35 +38,11 @@ function moneyCounter(purchaseItems){
   bitcoinProfitCount += purchaseTotal;
 }
 
-const getCpuStats = () => {
-  const cpus = os.cpus();
-  let idle = 0, total = 0;
-  cpus.forEach(cpu => {
-    for (let type in cpu.times) total += cpu.times[type];
-    idle += cpu.times.idle;
-  });
-  return { idle, total };
-};
 
-// Initialize the first snapshot
-let lastStats = getCpuStats();
-
-
-setInterval(() => {
-  const currentStats = getCpuStats();
-
-  const idleDiff = currentStats.idle - lastStats.idle;
-  const totalDiff = currentStats.total - lastStats.total;
-
-  if (totalDiff > 0) {
-    const usage = 100 - (100 * idleDiff / totalDiff);
-    // Update your local variable as a clean Number
-    currentCpuUsage = Number(usage.toFixed(2));
-  }
-
-  // Store current stats as "lastStats" for the next interval calculation
-  lastStats = currentStats;
-}, 9000); // Updates every 2 seconds
+function getCpuUsagePercentage() {
+  const cpuUsage = os.loadavg()[0] / os.cpus().length;
+  return cpuUsage.toFixed(2) * 100;
+}
 
 function getMemoryUsagePercentage() {
   const totalMemory = os.totalmem();
@@ -137,7 +113,7 @@ setInterval(() => {
   metrics.push(createMetric('failedPurchases', failedPurchaseCount, '1', 'sum', 'asInt', {}));
   metrics.push(createMetric('bitcoinRevenue', bitcoinProfitCount, 'BTC', 'sum', 'asDouble', {}));
   metrics.push(createMetric('activeUsers', Object.keys(activeUsers).length, '1', 'sum', 'asInt', {}));
-  metrics.push(createMetric('cpuUsage', parseInt(currentCpuUsage), '%', 'gauge', 'asInt', {}));
+  metrics.push(createMetric('cpuUsage', parseInt(getCpuUsagePercentage), '%', 'gauge', 'asInt', {}));
   metrics.push(createMetric('memoryUsage', parseInt(getMemoryUsagePercentage()), '%', 'gauge', 'asInt', {}));
   if (latencies.length > 0){
     let avgLatency = latencies.reduce((acc, curr) => acc + curr, 0) / latencies.length
